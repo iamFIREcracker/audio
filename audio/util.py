86e5c0539d7c3801081097fbae5fb4264bf5984d
Matteo Landi <landimatte@gmail.com>
2010-08-01 01:12:22 +0200

    Add module containing utility functions.
    The module containg a function which wrap the fft exported from numpy.

---
+++ audio/util.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: Latin-1 -*-
+
+"""Module containing utility function for audio processing.
+"""
+
+from __future__ import division
+
+import numpy
+
+
+def fft(data):
+    """Compute the fft of the input data.
+    
+    The returned list of values is normalized by a coefficient 2/N with N
+    number of input samples, and is relative to positive values of frequencies.
+    
+    Keywords:
+        data List of values bounded between -1 and +1.
+        
+    Return:
+        Normalized fft.
+    """
+    N = len(data)
+    return 2 / N * numpy.fft.fft(data)[:N // 2]
