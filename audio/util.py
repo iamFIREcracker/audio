8829d88903e6b5356ad214c49e77790a17e3e0b7
Matteo Landi <landimatte@gmail.com>
2010-08-12 10:32:15 +0200

    Add batch function to the module.
    The function has been moved here because needed by all the visualizer in order
    to reduce the volume of data displayed.

--- audio/util.py
+++ audio/util.py
@@ -5,11 +5,25 @@
 """
 
 from __future__ import division
+from itertools import islice
+from itertools import chain
 
 from numpy.fft import fft as _fft
 
         
-def _nextpow(x, b):
+def batch(iterable, size):
+    """Split the iterable into batchs of given size.
+    
+    Keywords:
+        iterable iterable to split.
+        size max size of each returned batch (last one could be smaller).
+    """
+    sourceiter = iter(iterable)
+    while True:
+        batchiter = islice(sourceiter, size)
+        yield list(chain([batchiter.next()], batchiter))
+            
+def nextpow(x, b):
     """Find the smallest integer p such that b ** p > x.
     """
     p = 1
@@ -29,7 +43,7 @@ def fft(data):
         Normalized fft.
     """
     N = len(data)
-    M = _nextpow(len(data), 2)
+    M = nextpow(len(data), 2)
     norm_factor = 2 / M
     data = norm_factor * _fft(data, M)
     return data[:N // 2]
