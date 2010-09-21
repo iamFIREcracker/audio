9ffa8c08a923c9490050933f488f3ae41f93b487
Matteo Landi <landimatte@gmail.com>
2010-09-21 22:30:16 +0200

    Remove white spaces from the end of the lines.

--- audio/source.py
+++ audio/source.py
@@ -14,38 +14,38 @@ import gst
 class Source(gobject.GObject):
     """Base audio source object.
     """
-    
+
     __gsignals__ = {
             'new-data': (gobject.SIGNAL_RUN_FIRST, None,
                          (gobject.TYPE_PYOBJECT,)),
     }
-    
+
     def __init__(self, pipeline, speakers, emit):
         """Constructor.
-        
+
         Keywords:
             pipeline string representing a gstreamer pipeline.
             speakers flag indicating wether to output data to the speakers.
             emit flag indicating wether to emit signals notifying new data.
         """
         super(Source, self).__init__()
-        
+
         self.pipeline = gst.parse_launch(pipeline)
         if not speakers:
             self.pipeline.get_by_name('asink').set_state(gst.STATE_NULL)
         if emit:
             self.pipeline.get_by_name('fsink').connect('handoff',
                                                        self.handoff_cb)
-            
+
     def handoff_cb(self, fakesink, buff, pad):
         """Invoked when the fakesink collected a new buffer of data.
-        
+
         The format of the input buffer is supposed to be:
             channels: mono
             samplerate: 44100
             bitspersample: 16 signed
             endianess: little
-            
+
         Emit a signal containing the array of data: the values are bounded
         between -1 and 1
         """
@@ -58,12 +58,12 @@ class Source(gobject.GObject):
             except KeyError:
                 print 'Caps not supported:', buff.caps
                 return
-            
-        samples = buff.size // 2 #16 bits per sample
+
+        samples = buff.size // 2 # 16 bits per sample
 
         fmt = "<" + str(samples) + "h"
         self.emit('new-data', [v / 32768 for v in unpack(fmt, buff.data)])
-    
+
     def start(self):
         """Start the pipeline.
         """
@@ -83,10 +83,10 @@ class Source(gobject.GObject):
 class Microphone(Source):
     """Microphone source object.
     """
-    
+
     def __init__(self, speakers=True, emit=False):
         """Constructor.
-        
+
         Keywords:
             speakers flag indicating wether to output data to the speakers.
             emit flag indicating wether to emit signals notifying new data.
@@ -110,10 +110,10 @@ class Microphone(Source):
 class Tone(Source):
     """Single tone source object.
     """
-    
+
     def __init__(self, speakers=True, emit=False):
         """Constructor.
-        
+
         Keywords:
             speakers flag indicating wether to output data to the speakers.
             emit flag indicating wether to emit signals notifying new data.
@@ -132,10 +132,10 @@ class Tone(Source):
                    fakesink name=fsink signal-handoffs=true sync=true t. !
                queue max-size-time=1000 !
                    autoaudiosink name=asink''', speakers, emit)
-        
+
     def set_values(self, freq, volume):
         """Set source properties.
-        
+
         Keywords:
             freq new frequency value bounded between -1 and +1.
             volume new volume value bounded between -1 and +1.
@@ -148,11 +148,10 @@ class Tone(Source):
 class AudioFile(Source):
     """Audio file source object.
     """
-                 
-    
+
     def __init__(self, location, speakers=True, emit=False):
         """Constructor.
-        
+
         Keywords:
             location location of the file to play
             speakers flag indicating wether to output data to the speakers.
